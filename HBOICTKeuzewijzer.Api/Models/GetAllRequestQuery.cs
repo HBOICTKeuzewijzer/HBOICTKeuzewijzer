481b6e38f3543b06bd20d23d4c2abfe77ed91532
baswindesheim <s1159500@student.windesheim.nl>
2025-04-25 11:01:03 +0200

    WIP

--- HBOICTKeuzewijzer.Api/Models/GetAllRequestQuery.cs
+++ HBOICTKeuzewijzer.Api/Models/GetAllRequestQuery.cs
@@ -2,24 +2,12 @@
 
 namespace HBOICTKeuzewijzer.Api.Models
 {
-    public class GetAllRequestQuery<T>
+    public class GetAllRequestQuery
     {
         public string? Filter { get; set; }
         public string? SortColumn { get; set; }
-        public Direction SortDirection { get; set; }
+        public string? SortDirection { get; set; }
         public int? Page { get; set; }
         public int? PageSize { get; set; }
-        public Expression<Func<T, object>>? SortExpression { get; set; }
-
-        public void SetSortExpression()
-        {
-            if (!string.IsNullOrEmpty(SortColumn))
-            {
-                var parameter = Expression.Parameter(typeof(T), "x");
-                var property = Expression.Property(parameter, SortColumn);
-                var conversion = Expression.Convert(property, typeof(object));
-                SortExpression = Expression.Lambda<Func<T, object>>(conversion, parameter);
-            }
-        }
     }
 }
