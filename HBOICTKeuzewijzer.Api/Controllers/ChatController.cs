423b84b2df7c62b11f8ad91097d1b96d7b37e903
Justin <justinvdmey@hotmail.com>
2025-05-01 18:42:10 +0200

    Init

---
+++ HBOICTKeuzewijzer.Api/Controllers/ChatController.cs
@@ -0,0 +1,45 @@
+﻿using HBOICTKeuzewijzer.Api.Models;
+using HBOICTKeuzewijzer.Api.Repositories;
+using HBOICTKeuzewijzer.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HBOICTKeuzewijzer.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IRepository<Chat> _chatRepository;
+        private readonly ApplicationUserService _userService;
+
+        public ChatController(IRepository<Chat> chatRepository, ApplicationUserService userService)
+        {
+            _chatRepository = chatRepository;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedResult<Chat>>> List([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _chatRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaginatedResult<Chat>>> Read([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _chatRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PaginatedResult<Chat>>> Create([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _chatRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PaginatedResult<Chat>>> Update([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _chatRepository.GetPaginatedAsync(request));
+        }
+    }
+}
---
+++ HBOICTKeuzewijzer.Api/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using HBOICTKeuzewijzer.Api.Models;
+using HBOICTKeuzewijzer.Api.Repositories;
+using HBOICTKeuzewijzer.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HBOICTKeuzewijzer.Api.Controllers
+{
+    [Route("Chat/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IRepository<Message> _messageRepository;
+        private readonly ApplicationUserService _userService;
+
+        public MessageController(IRepository<Message> messageRepository, ApplicationUserService userService)
+        {
+            _messageRepository = messageRepository;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedResult<Message>>> List([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _messageRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PaginatedResult<Message>>> Create([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _messageRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PaginatedResult<Message>>> Update([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _messageRepository.GetPaginatedAsync(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PaginatedResult<Message>>> Delete([FromQuery] GetAllRequestQuery request)
+        {
+            return Ok(await _messageRepository.GetPaginatedAsync(request));
+        }
+    }
+}
