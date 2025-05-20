a475ea1a48ac18784f4b9b7b5272e4ea30a5165b
Jarne Kompier <j.kompier@hotmail.nl>
2025-05-21 00:29:30 +0200

    Trials

---
+++ HBOICTKeuzewijzer.Tests.Integration/Fixtures/SharedTestCollection.cs
@@ -0,0 +1,10 @@
+﻿using HBOICTKeuzewijzer.Tests.Integration.Fixtures;
+
+namespace HBOICTKeuzewijzer.Tests.Integration.Fixtures
+{
+    [CollectionDefinition("Shared Test Collection")]
+    public class SharedTestCollection : ICollectionFixture<SharedDatabaseFixture>
+    {
+
+    }
+}
