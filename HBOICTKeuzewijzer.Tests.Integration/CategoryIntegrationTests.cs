035128ef216d5a136db94b8fd68ef72754377232
Jarne Kompier <j.kompier@hotmail.nl>
2025-05-25 14:15:36 +0200

    Test test removed

--- HBOICTKeuzewijzer.Tests.Integration/CategoryIntegrationTests.cs
+++ HBOICTKeuzewijzer.Tests.Integration/CategoryIntegrationTests.cs
@@ -36,34 +36,4 @@ public class CategoryIntegrationTests
         saved.Should().NotBeNull();
         saved!.Value.Should().Be("Test Category");
     }
-
-    [Theory]
-    [InlineData("SystemAdmin")]
-    [InlineData("ModuleAdmin")]
-    public async Task PostCategory_CreatesEntryInDatabase(string role)
-    {
-        using var factory = new TestAppFactory();
-
-        var newCategory = new Category
-        {
-            Id = Guid.NewGuid(),
-            Value = "Test Category",
-            Position = 1
-        };
-
-        var request = new HttpRequestMessage(HttpMethod.Post, "/Category")
-        {
-            Content = JsonContent.Create(newCategory)
-        };
-        request.Headers.Add("X-Test-Role", role);
-
-        var response = await factory.Client.SendAsync(request);
-
-        response.StatusCode.Should().Be(HttpStatusCode.Created);
-        response.Headers.Location!.AbsolutePath.Should().Be($"/Category/{newCategory.Id}");
-
-        var saved = await factory.DbContext.Categories.FindAsync(newCategory.Id);
-        saved.Should().NotBeNull();
-        saved!.Value.Should().Be("Test Category");
-    }
 }
