dc205128ccfbb19b3e237e142b86c3204ab79890
Baswindesheim <s1159500@student.windesheim.nl>
2025-05-27 15:02:21 +0200

    UserService wordt hlm niet gebruikt, stupid me

--- HBOICTKeuzewijzer.Api/Controllers/ModuleController.cs
+++ HBOICTKeuzewijzer.Api/Controllers/ModuleController.cs
@@ -1,129 +1,102 @@
 ﻿using HBOICTKeuzewijzer.Api.Attributes;
 using HBOICTKeuzewijzer.Api.Models;
 using HBOICTKeuzewijzer.Api.Repositories;
-using HBOICTKeuzewijzer.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace HBOICTKeuzewijzer.Api.Controllers
+namespace HBOICTKeuzewijzer.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ModuleController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ModuleController : ControllerBase
+    private readonly IRepository<Module> _moduleRepo;
+    public ModuleController(IRepository<Module> moduleRepo)
     {
-        private readonly IRepository<Module> _moduleRepo;
-        private readonly ApplicationUserService _userService;
+        _moduleRepo = moduleRepo;
+    }
 
-        public ModuleController(IRepository<Module> moduleRepo, ApplicationUserService userService)
+    // GET: api/Module
+    [HttpGet("paged")]
+    public async Task<ActionResult<PaginatedResult<Module>>> GetPagedModules(
+        [FromQuery] GetAllRequestQuery request)
+    {
+        var result = await _moduleRepo.GetPaginatedAsync(request, m => m.Category);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Module>>> GetModules()
+    {
+        var result = await _moduleRepo.GetAllIncludingAsync(m => m.Category);
+
+        return Ok(result);
+    }
+
+    [HttpGet("count")]
+    public async Task<ActionResult<int>> GetCount([FromQuery] string? filter = null)
+    {
+        var request = new GetAllRequestQuery { Filter = filter };
+        var result = await _moduleRepo.GetPaginatedAsync(request);
+        return Ok(result.TotalCount);
+    }
+
+    // GET: api/Module/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Module>> GetModule(Guid id)
+    {
+        var module = await _moduleRepo.GetByIdAsync(id);
+
+        if (module == null) return NotFound();
+
+        return module;
+    }
+
+    // PUT: api/Module/5
+    [HttpPut("{id}")]
+    [EnumAuthorize(Role.ModuleAdmin, Role.SystemAdmin)]
+    public async Task<IActionResult> PutModule(Guid id, Module module)
+    {
+        if (id != module.Id) return BadRequest("Id en url komt niet overeen met de module ID");
+
+        if (!await _moduleRepo.ExistsAsync(id)) return NotFound();
+
+        try
         {
-            _moduleRepo = moduleRepo;
-            _userService = userService;
+            await _moduleRepo.UpdateAsync(module);
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!await _moduleRepo.ExistsAsync(id)) return NotFound();
+
+            throw;
         }
 
-        // GET: api/Module
-        [HttpGet("paged")]
-        public async Task<ActionResult<PaginatedResult<Module>>> GetPagedModules(
-            [FromQuery] GetAllRequestQuery request)
-        {
-            var result = await _moduleRepo.GetPaginatedAsync(request, m => m.Category);
-            return Ok(result);
-        }
+        return CreatedAtAction(nameof(GetModule), new { id = module.Id }, module);
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Module>>> GetModules()
-        {
-            var result = await _moduleRepo.GetAllIncludingAsync(m => m.Category);
+    // POST: api/Module
+    [HttpPost]
+    [EnumAuthorize(Role.ModuleAdmin, Role.SystemAdmin)]
+    public async Task<ActionResult<Module>> PostModule(Module module)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            return Ok(result);
-        }
+        await _moduleRepo.AddAsync(module);
 
-        [HttpGet("count")]
-        public async Task<ActionResult<int>> GetCount([FromQuery] string? filter = null)
-        {
-            var request = new GetAllRequestQuery { Filter = filter };
-            var result = await _moduleRepo.GetPaginatedAsync(request);
-            return Ok(result.TotalCount);
-        }
+        return CreatedAtAction(nameof(GetModule), new { id = module.Id }, module);
+    }
 
-        // GET: api/Module/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Module>> GetModule(Guid id)
-        {
-            var module = await _moduleRepo.GetByIdAsync(id);
+    // DELETE: api/Module/5
+    [HttpDelete("{id}")]
+    [EnumAuthorize(Role.SystemAdmin, Role.ModuleAdmin)]
+    public async Task<IActionResult> DeleteModule(Guid id)
+    {
+        var module = await _moduleRepo.GetByIdAsync(id);
+        if (module == null) return NotFound();
 
-            if (module == null)
-            {
-                return NotFound();
-            }
+        await _moduleRepo.DeleteAsync(id);
 
-            return module;
-        }
-
-        // PUT: api/Module/5
-        [HttpPut("{id}")]
-        [EnumAuthorize(Role.ModuleAdmin, Role.SystemAdmin)]
-        public async Task<IActionResult> PutModule(Guid id, Module module)
-        {
-            if (id != module.Id)
-            {
-                return BadRequest("Id en url komt niet overeen met de module ID");
-            }
-
-            if (!await _moduleRepo.ExistsAsync(id))
-            {
-                return NotFound();
-            }
-
-            var user = await _userService.GetOrCreateUserAsync(User);
-
-            try
-            {
-                await _moduleRepo.UpdateAsync(module);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!await _moduleRepo.ExistsAsync(id))
-                {
-                    return NotFound();
-                }
-
-                throw;
-            }
-
-            return CreatedAtAction(nameof(GetModule), new { id = module.Id }, module);
-        }
-
-        // POST: api/Module
-        [HttpPost]
-        [EnumAuthorize(Role.ModuleAdmin, Role.SystemAdmin)]
-        public async Task<ActionResult<Module>> PostModule(Module module)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var user = await _userService.GetOrCreateUserAsync(User);
-
-            await _moduleRepo.AddAsync(module);
-
-            return CreatedAtAction(nameof(GetModule), new { id = module.Id }, module);
-        }
-
-        // DELETE: api/Module/5
-        [HttpDelete("{id}")]
-        [EnumAuthorize(Role.SystemAdmin, Role.ModuleAdmin)]
-        public async Task<IActionResult> DeleteModule(Guid id)
-        {
-            var module = await _moduleRepo.GetByIdAsync(id);
-            if (module == null)
-            {
-                return NotFound();
-            }
-
-            await _moduleRepo.DeleteAsync(id);
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
