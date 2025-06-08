597990d70701a427419e2748266f04104278e785
Baswindesheim <s1159500@student.windesheim.nl>
2025-06-08 23:05:43 +0200

    dto fix

--- HBOICTKeuzewijzer.Tests/Controllers/ChatControllerTests.cs
+++ HBOICTKeuzewijzer.Tests/Controllers/ChatControllerTests.cs
@@ -119,7 +119,7 @@ namespace HBOICTKeuzewijzer.Tests.Controllers
                 .ReturnsAsync((ApplicationUser)null);
 
             // Act
-            var result = await _controller.CreateWithEmail("test@example.com");
+            var result = await _controller.CreateWithEmail(new CreateChatDto{Email = "test@example.com" });
 
             // Assert
             var notFound = Assert.IsType<NotFoundObjectResult>(result.Result);
